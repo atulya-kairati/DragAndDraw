e67201cfe180df9a6eafdba1c1ff0be68235d965
atulya-kairati <atulyakairati@gmail.com>
2023-04-25 10:52:24 +0530

    added custom view

---
+++ app/src/main/java/com/atulya/draganddraw/customview/BoxDrawingView.kt
@@ -0,0 +1,13 @@
+package com.atulya.draganddraw.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(
+    context: Context,
+    attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+
+}
