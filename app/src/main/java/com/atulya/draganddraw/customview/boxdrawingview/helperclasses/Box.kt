7bf649505a710314ebd194da126e0dd3d178469a
atulya-kairati <atulyakairati@gmail.com>
2023-04-25 12:29:19 +0530

    fixed bug: left to right not drawing

--- app/src/main/java/com/atulya/draganddraw/customview/boxdrawingview/helperclasses/Box.kt
+++ app/src/main/java/com/atulya/draganddraw/customview/boxdrawingview/helperclasses/Box.kt
@@ -2,9 +2,7 @@ package com.atulya.draganddraw.customview.boxdrawingview.helperclasses
 
 import android.graphics.PointF
 
-data class Box(val start: PointF) {
-
-    var end: PointF = start
+data class Box(val start: PointF, var end: PointF = start) {
 
     val left: Float
         get() = Math.min(start.x, end.x)
@@ -13,7 +11,7 @@ data class Box(val start: PointF) {
         get() = Math.min(start.y, end.y)
 
     val right: Float
-        get() = Math.max(start.x, start.x)
+        get() = Math.max(start.x, end.x)
 
     val bottom: Float
         get() = Math.max(start.y, end.y)
